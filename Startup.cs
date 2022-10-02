6aea1933c55cb9ff631da566fbc64c5062bcb284
RiggityRussell <russellgtarlt@gmail.com>
2022-10-02 19:53:40 -0400

    Initial push

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,8 @@ namespace MovieList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Models.MovieContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+            services.AddRouting(options => { options.LowercaseUrls = true; options.AppendTrailingSlash = true; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
